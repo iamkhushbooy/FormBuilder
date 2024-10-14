19f826ec85b503ef29d7d7a3409d00b27bfa17d5
myselfkhushboo <khushboocsjm@gmail.com>
2024-10-14 14:08:18 +0530

    removed eslint

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
 const Header = () => {
   return (
     <div className='w-full h-[70px] bg-slate-400 text-center 
      font-semibold p-3sticky top-0 flex justify-center items-center'>
-         <Image
-        src='/form.png'
-        alt='Description of the image'
-        width={50}
-        height={50} 
-      />
       <h1 className='text-[30px]  text-white '>FORM BUILDER</h1>
         </div>
   )
